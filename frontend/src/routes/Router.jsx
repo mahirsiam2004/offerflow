ca0e410d4b013170c0be3a3f2fbe70e04c2efcdf
mahirsiam2004 <mahirsiam2004@gmail.com>
2026-07-12 22:50:16 +0600

    convert into react latest version

---
+++ frontend/src/routes/Router.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "../layoutes/RootLayout";
+import { Home } from "../pages/Home/Home";
+
+export const router =createBrowserRouter([
+    {
+        path:"/",
+        Component:RootLayout,
+        children:[
+            {
+                index:true,
+                Component:Home
+            }
+        ]
+    }
+])
